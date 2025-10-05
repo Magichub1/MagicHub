e5f53e2dee4ee1dce2229199936123f3c5dc3cab
Magichub1 <magichub@bk.ru>
2025-10-06 00:45:42 +0300

    Update MagicHub.lua

--- MagicHub.lua
+++ MagicHub.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://gist.githubusercontent.com/Magichub1/0eeceab991b54ded1d8ce26ef7f4e5f7/raw/d6ffcd51040719cab5e20476c817829580ea8ee1/MagicHub.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Magichub1/MagicHub/main/MagicHub.lua"))()
